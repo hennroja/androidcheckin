207a452542f8ea97815a11829018d34f005c79aa
Robin Kuck <robin@basicinside.de>
2013-11-08 21:30:44 +0100

    stuff

--- 42discipline/src/com/fortytwo/discipline/MainActivity.java
+++ 42discipline/src/com/fortytwo/discipline/MainActivity.java
@@ -1,18 +1,50 @@
 package com.fortytwo.discipline;
 
+import java.util.ArrayList;
+
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.ActivityManager.RunningServiceInfo;
 import android.content.Intent;
+import android.content.Context;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
+	ArrayList<String> appointment;
+	ArrayAdapter<String> aa;
+	MainActivity main = this;
+
+	private void addItem(String item) {
+
+		if (item.length() > 0) {
+			this.appointment.add(item);
+			this.aa.notifyDataSetChanged();
+		}
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		ListView lv = (ListView) findViewById(R.id.listView1);
+
+		appointment = new ArrayList<String>();
+		aa = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, appointment);
+		lv.setAdapter(aa);
+		
+		addItem("Desk");
+		addItem("Door");
+		addItem("Kitchen");
+		addItem("Testbed #1");
+		addItem("Testbed #2");
+		addItem("Testbed #3");
 		
 		ActivityManager aManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
 		
@@ -29,6 +61,31 @@ public class MainActivity extends Activity {
         if(!isRunning) {
         	startService(new Intent( new Intent(this, SSIDCheckerService.class)));
         }
+		Button btnScan = (Button) findViewById(R.id.button1);
+		btnScan.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				WifiManager wifiManager = (WifiManager) 
+						main.getSystemService(Context.WIFI_SERVICE);
+				wifiManager.startScan();
+			}
+			
+		});
+
+		ActivityManager aManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+		for (RunningServiceInfo service : aManager
+				.getRunningServices(Integer.MAX_VALUE)) {
+
+			if ("com.fortytwo.discipline.SSIDCheckerService.java"
+					.equals(service.service.getClassName())) {
+
+				System.out.println("HHEHEHEHEHE");
+
+				break;
+
+			}
+		}
 
 	}
 
